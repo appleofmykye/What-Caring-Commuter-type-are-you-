1aa754400adcd10ee89162558f159569cd31d603
appleofmykye <tkyelin@gmail.com>
2026-07-31 13:38:31 +0800

    Update CCCscript.js

--- CCCscript.js
+++ CCCscript.js
@@ -513,3 +513,29 @@ function showResult() {
 /* ---------- Start Quiz ---------- */
 
 loadQuestion();
+
+/* ---------- Restart Quiz ---------- */
+
+restartButton.addEventListener("click", () => {
+
+    currentQuestion = 0;
+
+    chosenAnswers = [];
+
+    scores = {
+        E: 0,
+        I: 0,
+        S: 0,
+        N: 0,
+        T: 0,
+        F: 0,
+        J: 0,
+        P: 0
+    };
+
+    resultScreen.style.display = "none";
+    quizContainer.style.display = "block";
+
+    loadQuestion();
+
+});
